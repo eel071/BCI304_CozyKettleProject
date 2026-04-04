3aa4da76bf94c6139bfae829d0200506ae5044fc
eel071 <e_l071@student.usc.edu.au>
2026-04-04 22:51:49 +1000

    Created Prefabs, Added Tags, Added Teapot Script
    Changed scene name to TeaStation.
    Added Tags to draggables so they would only interact with what they needed to.

--- BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/HotPlate.cs
+++ BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/HotPlate.cs
@@ -4,7 +4,16 @@ public class HotPlate : MonoBehaviour, IOnDropBaseCollision
 {
     public void OnDrop(Draggable draggable)
     {
-        Debug.Log("Water is Heating");
-        draggable.transform.position = transform.position + new Vector3(0,1,0);
-    }
+        if (draggable.tag == "Teapot")
+        {
+            Debug.Log($"Water is Heating");
+            draggable.transform.position = transform.position + new Vector3(0, 1, 0);
+        }        
+        else
+        {
+            Debug.Log($"Tried to Heat {draggable.tag}");
+            draggable.transform.position = draggable.startPosition;
+        }                
+    }   
+    
 }
--- BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/Teapot.cs
+++ BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/Teapot.cs
@@ -1,23 +1,19 @@
 using UnityEngine;
 
-public class Teacup : MonoBehaviour, IOnDropBaseCollision
+public class TeaPot : MonoBehaviour, IOnDropBaseCollision
 {
     public void OnDrop(Draggable draggable)
-    {        
-        if (draggable.tag == "Teapot")
-        {
-            Debug.Log($"Tea Filled");
-            draggable.transform.position = draggable.startPosition;
-        }        
-        else if(draggable.tag == "Addition")
+    {
+        if (draggable.tag == "Tea")
         {
             Debug.Log($"Added {draggable.gameObject.name}");
             Destroy(draggable.gameObject);
-        }
+        }        
         else
         {
             Debug.Log($"Tried to Add {draggable.tag}");
             draggable.transform.position = draggable.startPosition;
         }
     }
+
 }
