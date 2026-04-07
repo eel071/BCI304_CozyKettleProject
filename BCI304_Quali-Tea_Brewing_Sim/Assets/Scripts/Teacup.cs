89f1c3955c441ce39a1b02a78150a86977f0685b
KatieeeB <keb051@student.usc.edu.au>
2026-04-07 13:03:49 +1000

    Tea Cup Sprites
    - Imported the tea cup sprite sheet
    - Added sprites to the tea cup object
    - Edited tea cup script

--- BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/Teacup.cs
+++ BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/Teacup.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class Teacup : MonoBehaviour, IOnDropBaseCollision
 {
+    [SerializeField] private GameObject tea;
+    private float teaMax = 0.25f; 
+    private float fillLevel = 0f;
+    private Vector3 teaFill;
+    private Vector3 teaEmpty;
+    private bool filled = false;
+
+    void Start()
+    {
+        teaEmpty = tea.transform.position;
+    }
+
     public void OnDrop(Draggable draggable)
     {        
         if (draggable.tag == "Teapot")
         {
             Debug.Log($"Tea Filled");
+            if (!filled)
+            {
+                FillCup();
+            }
             draggable.transform.position = draggable.startPosition;
         }        
         else if(draggable.tag == "Addition")
@@ -20,5 +36,18 @@ public class Teacup : MonoBehaviour, IOnDropBaseCollision
             draggable.transform.position = draggable.startPosition;
         }
     }
-    
+
+    private void FillCup()
+    { 
+        filled = true;
+        //will have to change this later so it can have varying heights
+        //Mathf.Lerp(0, teaMax, fillLevel) 
+        tea.transform.position += new Vector3(0f, teaMax, 0f); 
+    }
+
+    private void EmptyCup()
+    {
+        tea.transform.position = teaEmpty;
+        filled = false;
+    }
 }
