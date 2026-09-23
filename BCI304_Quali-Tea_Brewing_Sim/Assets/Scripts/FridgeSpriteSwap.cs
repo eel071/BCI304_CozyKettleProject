67079b35eb96de0ddb4a4f7053ed3201e8f77223
Teeara <tpt004@student.usc.edu.au>
2026-09-23 13:38:25 +1000

    Milk Fridge Sprite
    Created and imported milk fridge sprite.
    On mouse hover, fridge opens.

---
+++ BCI304_Quali-Tea_Brewing_Sim/Assets/Scripts/FridgeSpriteSwap.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class FridgeSpriteSwap : MonoBehaviour
+{
+    public Sprite closedSprite;
+    public Sprite openSprite;
+
+    private SpriteRenderer fridgeRenderer;
+    private Collider2D fridgeCollider;
+    private bool isOpen = false;
+
+    void Awake()
+    {
+        fridgeRenderer = GetComponent<SpriteRenderer>();
+        fridgeCollider = GetComponent<Collider2D>();
+
+        Debug.Log("FridgeSpriteSwap loaded");
+    }
+
+    void Update()
+    {
+        if (fridgeRenderer == null || fridgeCollider == null)
+            return;
+
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePosition = new Vector2(mouseWorld.x, mouseWorld.y);
+
+        bool mouseInside = fridgeCollider.OverlapPoint(mousePosition);
+
+        if (mouseInside && !isOpen)
+        {
+            fridgeRenderer.sprite = openSprite;
+            isOpen = true;
+        }
+
+        if (!mouseInside && isOpen && !Input.GetMouseButton(0))
+        {
+            fridgeRenderer.sprite = closedSprite;
+            isOpen = false;
+        }
+    }
+}
